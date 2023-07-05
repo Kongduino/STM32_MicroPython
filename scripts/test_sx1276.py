a09d85bb812ebdb1959f39daf18fb476fe538fed
Kongduino <dbvic@me.com>
2023-07-05 17:02:11 +0800

    Added sx127x library and example

--- scripts/test_sx1276.py
+++ scripts/test_sx1276.py
@@ -1,14 +1,14 @@
-from machine import Pin, SPI, SoftSPI
+from machine import Pin, SoftSPI
+import time
 from sx127x import SX127x
 
 cs = Pin("D10", Pin.OUT)
 sck = Pin("D13", Pin.OUT)
 mosi = Pin("D11", Pin.OUT)
 miso = Pin("D12", Pin.IN)
-#spi = SoftSPI(1)
-spi2 = SoftSPI(sck=sck, mosi=mosi, miso=miso)
+spiBus = SoftSPI(baudrate=1666666, sck=sck, mosi=mosi, miso=miso)
 
-lora = SX127x(spi=spi2, pin_ss=cs)
+lora = SX127x(spi = spiBus, pin_ss=cs)
 message = ''
 pingCounter = 0
 
@@ -23,7 +23,7 @@ def PING():
     message = "Sent "+payload
     lora.print(bytearray(payload.encode()))
     pingCounter += 1
-    print("Sent")
+    print("Sent\n")
 
 v = Version()
 message = "Version: "+hex(v)
@@ -35,4 +35,12 @@ print(message)
 lora.init({'frequency' : 145000000, 'tx_power_level': 20, 'signal_bandwidth': 7,
            'spreading_factor': 12, 'coding_rate': 8, 'preamble_length': 8,
            'implicitHeader'  : False, 'sync_word': 0x12, 'enable_CRC': False})
+
 PING()
+target = time.ticks_ms() + 15000
+
+while True:
+    if time.ticks_ms() >= target:
+        PING()
+        target = time.ticks_ms() + 15000
+
